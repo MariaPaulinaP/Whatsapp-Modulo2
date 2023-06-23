34de86dbaad68ea949fb3eee2a7b75670497eb12
sebastianbarrerah <sebasbh2008@hotmail.com>
2023-06-22 20:54:33 -0500

    pagina contacto

---
+++ scripts/service/contactos.js
@@ -0,0 +1,12 @@
+import { APPIS } from "../modulos/appi.js";
+
+const trayendoContactos = async() => {
+    const {data} = axios.get(URL_USUARIOS)
+    data.forEach(element => {
+        
+        
+    });
+}
+
+
+
