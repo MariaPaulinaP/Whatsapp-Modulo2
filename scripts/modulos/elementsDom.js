184b6d217e9cbfcf80d88cfbf899a402a6b4efbb
Maria Paulina Parra <mariapaulinap0531@gmail.com>
2023-06-24 10:40:05 -0500

    pintar en html y diseños css tarjetas

--- scripts/modulos/elementsDom.js
+++ scripts/modulos/elementsDom.js
@@ -1,7 +1,7 @@
 export const BotonInicio = document.getElementById('btn__iniciarSesion');
 export const contenedorBienvenido = document.getElementById('contenedor__bienvenido');
 export const contenedorInicioSesion = document.getElementById('contenedor__inicioSesion');
-export const contenedorHome = document.getElementById('contenedor__home');
+export const contenedorHome = document.querySelector('.contenedor__home')
 
 
 
